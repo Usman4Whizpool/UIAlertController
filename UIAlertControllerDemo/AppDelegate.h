46a9eff6662700f26553a9adc4a1e21ee9a64b8e
Usman Nisar <usman.nisar@whizpool.com>
2014-09-22 17:58:46 +0500

    Initial Commit

---
+++ UIAlertControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIAlertControllerDemo
+//
+//  Created by Z's iMac on 22/09/2014.
+//  Copyright (c) 2014 Usman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIAlertControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIAlertControllerDemo
+//
+//  Created by Z's iMac on 22/09/2014.
+//  Copyright (c) 2014 Usman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
