225c8fffaa8b31e78ab622f63e6fe95b8d4f7f13
Usman Nisar <ghani.nisar@gmail.com>
2014-09-23 10:28:59 +0500

    Initial Code

--- UIAlertControllerDemo/ViewController.h
+++ UIAlertControllerDemo/ViewController.h
@@ -11,5 +11,7 @@
 @interface ViewController : UIViewController
 
 
+-(IBAction)OnClickChooseColor:(id)sender;
+
 @end
 
--- UIAlertControllerDemo/ViewController.m
+++ UIAlertControllerDemo/ViewController.m
@@ -24,4 +24,44 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(IBAction)OnClickChooseColor:(id)sender
+{
+    NSArray *pColorsArray = [[NSArray alloc] initWithObjects:@"Red", @"Blue",@"Green",@"White", @"Yellow", nil];
+    UIAlertController *alertView = [UIAlertController alertControllerWithTitle:@"UIAlertController Demo" message:@"Chosse Color" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    for(int nIndex = 0; nIndex < [pColorsArray count]; nIndex++)
+    {
+        [alertView addAction:[UIAlertAction actionWithTitle:[pColorsArray objectAtIndex:nIndex] style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            
+            NSString *csSelectedColor = [pColorsArray objectAtIndex:nIndex];
+            
+            if([csSelectedColor isEqualToString:@"Red"])
+            {
+                [self.view setBackgroundColor:[UIColor redColor]];
+            }
+            else if([csSelectedColor isEqualToString:@"Blue"])
+            {
+                [self.view setBackgroundColor:[UIColor blueColor]];
+            }
+            else if([csSelectedColor isEqualToString:@"Green"])
+            {
+                [self.view setBackgroundColor:[UIColor greenColor]];
+            }
+            else if([csSelectedColor isEqualToString:@"White"])
+            {
+                [self.view setBackgroundColor:[UIColor whiteColor]];
+            }
+            else if([csSelectedColor isEqualToString:@"Yellow"])
+            {
+                [self.view setBackgroundColor:[UIColor yellowColor]];
+            }
+            
+        }]];
+    }
+    
+    [alertView addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+    
+    [self presentViewController:alertView animated:YES completion:nil];
+}
+
 @end
